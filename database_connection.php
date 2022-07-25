c87cf2c49700fdcfdfc9235f6d07db5ddec65509
Sreeradha M <71545079+sreeradham@users.noreply.github.com>
2022-07-25 08:03:10 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,5 +2,5 @@
 //database_connection.php
 $connect = new PDO('mysql:host=localhost;dbname=project1', 'root', '123456789');
 $conn= mysqli_connect('localhost','root','123456789');
-$db=mysqli_select_db($conn,'s6_project');
+$db=mysqli_select_db($conn,'project1');
 ?>
